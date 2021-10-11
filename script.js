c47864d690491ec2eee2cd5feaffcd8550adb496
Ryan Contento <contento.rk@gmail.com>
2021-10-11 06:32:32 -0400

    setting values

--- script.js
+++ script.js
@@ -5,3 +5,6 @@ document.querySelector('.message').textContent = 'Correct Number!';
 
 document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 19;
+
+console.log(document.querySelector('.guess').value);
+document.querySelector('.guess').value = 23;
