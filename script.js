177f38033eb9c3f2b1671505d6119c82d8726368
Ryan Contento <contento.rk@gmail.com>
2021-10-11 20:17:31 -0400

    rules for no guess

--- script.js
+++ script.js
@@ -11,6 +11,10 @@ console.log(document.querySelector('.guess').value);
 */
 
 document.querySelector('.check').addEventListener('click', function () {
-  const guess = document.querySelector('.guess').value;
-  console.log(guess);
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'Not A Number';
+  }
 });
