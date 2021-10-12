9420867f25343819c8041c2b16d779331851042a
Ryan Contento <contento.rk@gmail.com>
2021-10-12 06:30:30 -0400

    add random number

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ document.querySelector('.score').textContent = 19;
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
-
+const number = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = number;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
