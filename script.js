793872f41044883e304178c30aea570a246a51a1
Ryan Contento <contento.rk@gmail.com>
2021-10-12 07:05:05 -0400

    basic rules added

--- script.js
+++ script.js
@@ -27,10 +27,16 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = score;
     } else {
       document.querySelector('.message').textContent = 'You Lost';
+      document.querySelector('.score').textContent = 0;
     }
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = 'Too Low';
-    score--;
-    document.querySelector('.score').textContent = score;
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too Low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You Lost';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
