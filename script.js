882d9a8f7d5a69e60d3fd4db461573bfcb3bea6e
Ryan Contento <contento.rk@gmail.com>
2021-10-12 06:37:58 -0400

    add event for correct guess

--- script.js
+++ script.js
@@ -9,13 +9,15 @@ document.querySelector('.score').textContent = 19;
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
-const number = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = number;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = 'Not A Number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number';
   }
 });
