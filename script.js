e2af6bddde99ece55d74c33130a8b90304609ea9
Ryan Contento <contento.rk@gmail.com>
2021-10-15 19:15:31 -0400

    add winning annimation

--- script.js
+++ script.js
@@ -24,6 +24,10 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number';
 
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
     // guess too high
   } else if (guess > secretNumber) {
     if (score > 1) {
