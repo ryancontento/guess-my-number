1714b5adc45a65438fddec8b66f4bf3d483d34e7
Ryan Contento <contento.rk@gmail.com>
2021-10-15 19:56:22 -0400

    reformat

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ console.log(document.querySelector('.guess').value);
 */
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
@@ -24,8 +23,8 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number';
 
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
-
     document.querySelector('.number').style.width = '30rem';
 
     // guess too high
